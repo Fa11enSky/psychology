7fbc39901f7dc92e29bd3853613396f4fdffcb64
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-09 23:44:54 +0300

    add header to layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
+import Header from '../Header/Header'
 const Layout = () => {
   return (
-    <div>
-          <header>header</header>
+    <div style={{ marginLeft: "auto",marginRight:"auto", maxWidth: "1440px",paddingLeft: 128,paddingRight:128}}>
+          <Header/>
           <Outlet/>
     </div>
   )
