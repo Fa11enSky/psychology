e256bdeced19b26fc1fb0ac4e4f77dbe0cc1ed0f
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-09 22:01:26 +0300

    init react-router & add layout

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
+import Psychologists from "./Pages/Psychologists";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="psychologists" element={<Psychologists/>}/>
+          <Route path="favorites" element={<Favorites/>} />
+        </Route>
+        
+        <Route path="*" element={<div>NF</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+const Layout = () => {
+  return (
+    <div>
+          <header>header</header>
+          <Outlet/>
+    </div>
+  )
+}
+
+export default Layout
