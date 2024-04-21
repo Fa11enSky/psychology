39acabdc5da9b7703fc48a96d4f6fed3530e480f
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-21 18:52:56 +0300

    init firebase
    import firebase config

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import  "firebase/auth";
+import "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1lypdvAVBBcKQvOsfkrbIITOV4-CFDiM",
+  authDomain: "psychologists-91c74.firebaseapp.com",
+  databaseURL:
+    "https://psychologists-91c74-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "psychologists-91c74",
+  storageBucket: "psychologists-91c74.appspot.com",
+  messagingSenderId: "739207982900",
+  appId: "1:739207982900:web:81894ef0d97a3848cebf61",
+  measurementId: "G-83WG14Y99G",
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth=getAuth()
+ getAnalytics(app);
